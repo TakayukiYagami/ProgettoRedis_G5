95b61bf99b5c35b93a860801ea6d0f1953cf3bb4
Giuseppe Base <g.base@itsrizzoli.it>
2023-12-26 13:24:34 +0100

    sviluppato il main, aggiunti commenti alle nuove funzioni, aggiunte specifiche alle funzioni da completare

--- funzioni.py
+++ funzioni.py
@@ -21,20 +21,20 @@ def registrati(username: str, password: str, nome: str, cognome: str, redis_conn
 
 
 def login(username: str, password: str, redis_conn: redis.Redis) -> str or bool:  # BASE
+    # l'utente passa username e password, il database viene interrogato per vedere se sono presenti
+    # in caso siano presenti restituisce l'id (chiave)
+    # restituisce l'id_utente se ha corrispondenza, altrimenti restituisce false
     chiave_utente = f'UTENTE:{username}'
     password_redis = redis_conn.hget(chiave_utente, 'Password')
     if password_redis == password:
         return username
-    # l'utente passa username e password, il database viene interrogato per vedere se sono presenti
-    # in caso siano presenti restituisce l'id (chiave)
-    # restituisce l'id_utente se ha corrispondenza, altrimenti restituisce false
     return False
 
 
-def trova_utenti(username: str) -> list:  # GABRIEL
+def trova_utenti(username: str, redis_conn: redis.Redis) -> list:  # GABRIEL
     # l'utente passe un username, anche parziale, e verranno restituiti tutti gli username
     # anche parzialmente compatibili
-    # restutuisce una lista di username
+    # restutuisce una lista di username o una lista vuota se non trova nulla
     ...
 
 
@@ -42,20 +42,10 @@ def aggiungi_utente(username_utente: str, username_contatto: str, redis_conn: re
     # viene passato un username e viene aggiunto alla lista dei contatti.
     # restituisce vero se tutto va a buon fine
     chiave_contatti = f'CONTATTI_UTENTE:{username_utente}'
-
-    chiave_utente = f'UTENTE:{username_utente}'
-    if not redis_conn.exists(chiave_utente):  # utente principale esiste?
-        print(f"L'utente {username_utente} non esiste.")
-        return False
-
     chiave_contatto = f'UTENTE:{username_contatto}'  # utente da aggiungere esiste?
     if not redis_conn.exists(chiave_contatto):
-        print(f"L'utente {username_contatto} non esiste.")
         return False
-
-    redis_conn.sadd(chiave_contatti, username_contatto)  # sadd gestisce già caso in cui siano già amici
-
-    print(f"{username_contatto} è stato aggiunto alla lista dei contatti di {username_utente}.")
+    redis_conn.sadd(chiave_contatti, username_contatto)  # sadd gestisce già caso in cui siano già amic*
     return True
 
 
@@ -65,7 +55,6 @@ def cambia_stato(username: str, redis_conn: redis.Redis) -> bool:
     chiave_utente = f'UTENTE:{username}'
 
     if not redis_conn.exists(chiave_utente):
-        print(f"L'utente {username} non esiste.")
         return False
 
     stato_corrente = redis_conn.hget(chiave_utente, 'DnD')
@@ -74,9 +63,7 @@ def cambia_stato(username: str, redis_conn: redis.Redis) -> bool:
     else:
         nuovo_stato = 'True'
     redis_conn.hset(chiave_utente, 'DnD', nuovo_stato)
-
-    print(f"Lo stato di non disturbare per l'utente {username} è stato cambiato a {nuovo_stato}.")
-    return True
+    return nuovo_stato
 
 
 def ottieni_stato(username: str, redis_conn: redis.Redis) -> bool:
@@ -112,12 +99,13 @@ def ottieni_contatti(username: str, redis_conn: redis.Redis) -> list:
 def messaggi(mittente: str, destinatario: str, testo: str, redis_conn: redis.Redis) -> int:  # BASE
     # l'untente inserisce il destinatario, ed il testo.
     # restituisce 2 se va tutto bene, restituisce 1 se l'utente è occupato
+    # controllo sullo stato del destinatario
     if not ottieni_stato(destinatario, redis_conn):
         return 1
     chiave = sorted([mittente, destinatario])
-    print(chiave)
+    # creazione del testo composto da: 26 caratteri relativi al timestamp, 1 carattere relativo alla posizione sulla lista
+    # del mittente e N caratteri desitinati al testo
     testo_metadati = str(int(time.time())) + str(chiave.index(mittente)) + testo
-    print(testo_metadati)
     chiave_esiste = redis_conn.exists(f'{chiave[0]}{chiave[1]}')
     if not chiave_esiste:
         redis_conn.rpush(f'{mittente}{destinatario}', testo_metadati)
@@ -125,6 +113,8 @@ def messaggi(mittente: str, destinatario: str, testo: str, redis_conn: redis.Red
     return 2
 
 
-def leggi_messaggi(mittente: str, destinatario: str) -> list:  # CARLOTTA
+def leggi_messaggi(mittente: str, destinatario: str, redis_conn: redis.Redis) -> list or bool:  # CARLOTTA
     # restituisce tutti i valori da tra due utenti
+    # ogni elemento della lista è un testo mandato da un utente
+    # restituisce False se non trova la chat tra i due utenti
     ...
--- main.py
+++ main.py
@@ -1,29 +1,17 @@
 import funzioni
 import redis
 
-# Assuming id_utente is initially set to None or some default value
-id_utente = 1
+id_utente = 'lucabarbetta'
 
 if __name__ == '__main__':
 
     r = redis.Redis(
-                host= "redis-10797.c55.eu-central-1-1.ec2.cloud.redislabs.com",
-                port=10797,
-                password="yGoGfZmUmeAy90OLyd27DoVDyXgeq95L",
-                decode_responses=True
-                )
-    
+        host="redis-10797.c55.eu-central-1-1.ec2.cloud.redislabs.com",
+        port=10797,
+        password="yGoGfZmUmeAy90OLyd27DoVDyXgeq95L",
+        decode_responses=True
+    )
 
-#esempi per testare le funzioni poi da cancellare
-    funzioni.registrati("lucerna23", "cantolibero", "Lucio", "Battisti", r)
-    funzioni.aggiungi_utente("lucabarbetta", "lucerna23", r)
-    funzioni.cambia_stato("giovannithebest", r)
-    funzioni.ottieni_stato("giovannithebest", r)
-
-
-#IMPORTANTE
-#qui sotto manca il caso in cui si vuole ottenere lo stato dell'altro utente (se DND o no)
-            
     while True:
         print('Benvenuti nel sistema di messaggistica di redis')
         print('Seleziona una delle seguenti opzioni:')
@@ -41,12 +29,73 @@ if __name__ == '__main__':
                 except:
                     print('Non hai inserito un valore valido')
                 if sel == 1:
+                    print('Arrivederci')
                     id_utente = None
+                    r.close()
                     break
+                if sel == 2:
+                    stato = funzioni.cambia_stato(id_utente, r)
+                    if stato:
+                        print(f'Stato cambiato con successo, stato attuale: {stato}')
+                if sel == 3:
+                    username_contatto = input('Inserisci username da cercare (anche parziale)')
+                    utenti = funzioni.trova_utenti(username=username_contatto, redis_conn=r)
+                    if utenti:
+                        for elem in utenti:
+                            print(elem)
+                    else:
+                        print('Nessun utente trovato')
+                if sel == 4:
+                    username_contatto = input('Inserisci username: ')
+                    if funzioni.aggiungi_utente(username_utente=id_utente, username_contatto=username_contatto,
+                                                redis_conn=r):
+                        print(f'{username_contatto} aggiunto con successo')
+                    else:
+                        print('Username non trovato')
+                if sel == 5:
+                    contatti = funzioni.ottieni_contatti(id_utente, r)
+                    id_destinatario = ''
+                    if contatti:
+                        for i, elem in enumerate(contatti):
+                            print(f'{i + 1}) {elem}')
+                        while 1:
+                            id_destinatario = input('Seleziona contatto')
+                            try:
+                                id_destinatario = int(id_destinatario)
+                                break
+                            except:
+                                print('Non hai inserito un valore valido')
+                    id_destinatario = contatti[id_destinatario - 1]
+                    messaggio = input('inserisci il messaggio:')
+                    stato = funzioni.messaggi(mittente=id_utente, destinatario=id_destinatario, testo=messaggio,
+                                              redis_conn=r)
+                    if stato == 1:
+                        print('Messaggio non recapitato, utente in DnD')
+                    elif stato == 2:
+                        print('Messaggio recapitato con successo')
+                if sel == 6:
+                    contatti = funzioni.ottieni_contatti(id_utente, r)
+                    id_destinatario = ''
+                    if contatti:
+                        for i, elem in enumerate(contatti):
+                            print(f'{i + 1}) {elem}')
+                        while 1:
+                            id_destinatario = input('Seleziona contatto')
+                            try:
+                                id_destinatario = int(id_destinatario)
+                                break
+                            except:
+                                print('Non hai inserito un valore valido')
+                    id_destinatario = contatti[id_destinatario - 1]
+                    print('Caricamento messaggi...')
+                    chat = funzioni.leggi_messaggi(mittente=id_utente, destinatario=id_destinatario, redis_conn=r)
+                    for elem in chat:
+                        print(elem)
         else:
             while 1:
-                print('1. Registrati')
-                print('2. Accedi')
+                print('1. Esci')
+                print('2. Registrati')
+                print('3. Accedi')
                 sel = input('')
                 try:
                     sel = int(sel)
@@ -54,22 +103,27 @@ if __name__ == '__main__':
                 except:
                     print('Non hai inserito un valore valido')
             if sel == 1:
+                print('Arrivederci')
+                r.close()
+                break
+            if sel == 2:
                 while 1:
                     user = input('Inserisci username: ')
                     pw = input('Inserisci password: ')
-                    out = funzioni.registrati(username=user, password=pw)
+                    out = funzioni.registrati(username=user, password=pw, redis_conn=r)
                     if out:
                         print('Registrazione avvenuta con successo')
                         break
                     print('Username già preso')
                     break
-            if sel == 2:
+            if sel == 3:
                 while 1:
                     user = input('Inserisci username: ')
                     pw = input('Inserisci password: ')
-                    out = funzioni.login(username=user, password=pw)
+                    out = funzioni.login(username=user, password=pw, redis_conn=r)
                     if out:
                         id_utente = out
                         break
                     print('Username o Password sbagliati')
                     break
+    r.close()
